ba1ce28d95018b696ead539ff160a852dbefa863
Yonatan <yonatan@Yonatans-Mac.local>
2014-03-25 09:52:25 +0200

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Get Test
+//
+//  Created by Yonatan on 3/25/14.
+//  Copyright (c) 2014 Yonatan Attias. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
